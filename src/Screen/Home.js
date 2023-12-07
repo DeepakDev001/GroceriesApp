2c3a56c3e0e8f8aac6d9879d95479e08b93c4967
DeepakRedsel <deepakkumar.redsel@gmail.com>
2023-12-07 17:51:54 +0530

    update

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -3,15 +3,28 @@ import { SafeAreaView } from 'react-native'
 import HomeIcon from '../Components/HomeIcon'
 import { StatusBar } from 'expo-status-bar'
 import HomeSearch from '../Components/HomeSearch'
+import HomeBanner from '../Components/HomeBanner'
+import ProjectTitle from '../Components/ProjectTitle'
+import Product from '../Components/Product'
 
 const Home = () => {
 
   //=============== main_return_function ===========//
   return (
-    <SafeAreaView style={{ paddingHorizontal: 20, paddingTop: 50 }}>
+    <SafeAreaView style={{
+      paddingHorizontal: 20,
+      paddingTop: 50,
+      flex: 1,
+      gap: 20,
+      backgroundColor: "white"
+    }}>
       <StatusBar style='white' />
       <HomeIcon />
       <HomeSearch />
+      <HomeBanner />
+      <ProjectTitle title="Exclusive Offer" />
+      <Product />
+      <ProjectTitle title="Best Selling" />
     </SafeAreaView>
   )
 }
